baf9a2ff92dbdf60544af93ffa1ba74393e28cbd
Zaid Tariq <zaidtariqshah>
2025-11-10 21:01:04 +0530

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <Link href="/" className="logo">
+          <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
+          <p>DevEvent</p>
+        </Link>
+        <ul>
+          {" "}
+          <Link href="/">Home</Link>
+        </ul>
+        <ul>
+          {" "}
+          <Link href="/">Events</Link>
+        </ul>
+        <ul>
+          {" "}
+          <Link href="/">Create Events</Link>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
